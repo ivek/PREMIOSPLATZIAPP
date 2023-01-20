c152013ce7e9c205f75c2a9275c1892aca98effc
ivek <ivekdelfin@gmail.com>
2023-01-20 14:04:46 -0600

    pruebas de views

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from datetime import timezone
 from django.db import models
 
 # Create your models here.
@@ -5,7 +8,15 @@ class Question(models.Model):
     questions_text = models.CharField(max_length= 200 )
     pub_date= models.DateField('date published')
 
+    def __str__(self):
+        return self.questions_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length= 200)
     vote = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    return HttpResponse ('HELLO WORLD')
+    return HttpResponse ('Estás en la página principal de Premios Platzi App')
+
+
+def detail(request, questions_id):
+    return HttpResponse(F"Estás viendo la pregunta {questions_id}"  )
+
+
+def results(request, questions_id):
+    return HttpResponse(F"Estás viendo la pregunta {questions_id}"  )
+
+
+def vote(request, questions_id):
+    return HttpResponse(F"Estás votando a la pregunta  {questions_id}"  )
