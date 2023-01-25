ab58bbcbf8f697411b63c94799574f03ca41f99f
ivek <ivekdelfin@gmail.com>
2023-01-25 12:31:43 -0600

    configuración index.html con render

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,15 @@
+from .models import Question 
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index (request):
-    return HttpResponse ('Estás en la página principal de Premios Platzi App')
+def index(request):
+    lastest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        # * La variable ahora está disponible en index.html
+        "lastest_question_list": lastest_question_list
+    })
 
 
 def detail(request, questions_id):
