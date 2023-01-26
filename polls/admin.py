7040023277820e1e69a292a4fb615ee62461d7de
ivek <ivekdelfin@gmail.com>
2023-01-26 14:07:04 -0600

    configuración de los list en el admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
-from polls.models import Choice, Question
-
-#admpliar admin
-
-
 # Register your models here.
+from .models import Question, Choice
 
-admin.site.register([Question,Choice])
+# clase para listar nuevas respuestas
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "questions_text"]
+    inlines = [ChoiceInLine] # se agrega la clase antes creada
+    list_display=("questions_text","pub_date")
+    list_filter= ["pub_date"]
+    search_fields=["questions_text"]
+
+
+admin.site.register(Question, QuestionAdmin) # los argumentos recibidos son 
+# el modelo de db y el modelo de vista
