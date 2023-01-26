c311814e6428d124e12c829d311667c81fa1788d
ivek <ivekdelfin@gmail.com>
2023-01-25 23:48:07 -0600

    se creó el test de questions

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class QuestionIndexView(TestCase):
+    def test_no_questions(self):
+        '''if no question exist, an appropiate message is displayed'''
+        response= self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No existen encuestas")
+        self.assertQuerysetEqual(response.context["lastest_question_list"], [])
